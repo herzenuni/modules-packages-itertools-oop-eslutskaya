a2b778152e0f15658374ccd069383e93a51e6220
karlittocute <kapitalina.ignatova@gmail.com>
2016-07-29 16:31:58 +0300

    Все хорошо
    Теперь в конструктор класса Graphics передается объект класса Game
    Создано меню
    Сделан return у метода test класса Game

--- graphics.py
+++ graphics.py
@@ -19,24 +19,67 @@ class Graphics:
         self.rect_size = canvas_size / self.n           # Размер квадрата
         self.grid_width = 3                             # Толщина решетки
 
-        self.canvas = Canvas(self.root, width = self.canvas_size, height = self.canvas_size, bg = "white")
-        self.canvas.pack()
+        self.made_menu()                                # Создаем меню
+        self.made_input()                               # Ввод имен 
+
+        self.frame_canvas = Frame(self.root)
+        self.canvas = Canvas(self.frame_canvas, width = self.canvas_size, height = self.canvas_size, bg = "white")
+        self.canvas.grid()
+        self.frame_canvas.grid()
+        self.new_game()
+        
+    def made_menu(self):
+        m = Menu(self.root)                            
+        self.root.config(menu = m)
+        fm = Menu(m)
+        m.add_cascade(label = "Game", menu = fm)
+        fm.add_command(label = "New Game", command = self.new_game)
+        fm.add_command(label = "Close game", command = self.close_game)
+
+    def made_input(self):
+        self.frame_player = Frame(self.root)
+        self.player_1 = Label(self.frame_player, text = "Enter Player 1 Name")        
+        self.player_inp_1 = Entry(self.frame_player, width = 20, bd = 3)
+        self.player_2 = Label(self.frame_player, text = "Enter Player 2 Name")        
+        self.player_inp_2 = Entry(self.frame_player, width = 20, bd = 3)
+        self.go = Button(self.frame_player, text = "Go", command = self.go)
+
+        self.player_1.grid()
+        self.player_inp_1.grid()
+        self.player_2.grid()
+        self.player_inp_2.grid()
+        self.go.grid()
+        self.frame_player.grid()
+
+    def new_game(self):
+        self.game.new_game(self.game.whos_turn, self.game.path_to_stat_file, self.game.field_size, self.game.stat)
+        self.frame_canvas.grid_forget()
+        self.frame_player.grid()
         self.draw_rect()                                # Рисует квадрат
         self.draw_grid()                                # Рисует решетку
-        
         self.canvas.tag_bind('rect', '<Button-1>',self.press)
         #Все виджеты с тегом "rect" при нажатии левой кнопки мыши, будут выполнять команду press
+
+
+    def go(self):
+            self.game.winner = None
+            self.game.player_1 =self.player_inp_1.get()
+            self.game.player_2 =self.player_inp_2.get()
+            print(self.game.player_1,self.game.player_2)
+            self.frame_player.grid_forget()
+            self.frame_canvas.grid()
 
     def press(self,event):
         i = event.y // self.rect_size
         j = event.x // self.rect_size
-        self.move = self.game.press(int(i),int(j))            # Узнаем свободна ли клетка и отсутствует ли победитель
+        self.move = self.game.press(int(i),int(j))             # Узнаем свободна ли клетка и отсутствует ли победитель
         if self.move == 1:
-                self.draw_tick(event.x,event.y,self.rect_size) #Передаем координаты курсора при нажатии на один из квадратов и размер клетки
+                self.draw_tick(event.x,event.y,self.rect_size) # Передаем координаты курсора при нажатии на один из квадратов и размер клетки
         elif self.move == 0:
                  self.draw_toe(event.x,event.y,self.rect_size) #Передаем координаты курсора при нажатии на один из квадратов и размер клетки
                 
-
+    def close_game(self):
+            self.root.destroy()
 
     def draw_grid(self):
         for i in range(1,self.n):
